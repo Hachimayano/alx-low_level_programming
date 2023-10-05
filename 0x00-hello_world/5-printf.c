64a2200160c1c3ff25ac748b703053af92f1f74d
Hachimayano <safopapa279@gmail.com>
2023-10-05 16:37:37 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints a line with print function
+ *
+ * Return: 0 when successfull
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
