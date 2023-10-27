76a30163ec2760d95fbf820b7fcd735f28a0d8b8
Hachimayano <safopapa279@gmail.com>
2023-10-27 16:23:22 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,10 +9,8 @@
  * Return: Always zero
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
-	(void) argc;
 	printf("%s\n", argv[0]);
-
 	return (0);
 }
